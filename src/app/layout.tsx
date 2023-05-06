9d4fe2f7966b84924eb5d9bfa5e9d6c53782f1b4
Adham Tarek <adhamtarek291@gmail.com>
2023-05-06 15:24:21 +0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import { Nunito } from "next/font/google";
+
+import "./globals.css";
+
+const nunito = Nunito({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Airbnb",
+  description: "airbnb clone with nextjs and tailwindcss and typescript",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={nunito.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import Test from "./Burkina-Faso.svg";
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      Hello airbnb! <Test className="h-10 w-10" />
+    </main>
+  );
+}
