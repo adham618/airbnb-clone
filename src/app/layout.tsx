31d2b1a35c2be90e6d2b85af14a4d2680f7ed971
Adham Tarek <adhamtarek291@gmail.com>
2023-05-10 16:11:13 +0300

    feat: fix seo tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 
@@ -7,11 +8,80 @@ import Footer from "@/components/Layout/Footer";
 import Header from "@/components/Layout/Header";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
-import Seo from "@/components/Seo";
 
 import getCurrentUser from "@/actions/getCurrentUser";
+import { siteConfig } from "@/config/site";
 
 const nunito = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  applicationName: siteConfig.name,
+  generator: `Next.js`,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+  ],
+  authors: [
+    {
+      name: "adham-tarek",
+      url: "https://adhamtarek.vercel.app",
+    },
+  ],
+  creator: "adham-tarek",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@test",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon/favicon-16x16.png",
+    apple: "/favicon/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/favicon/site.webmanifest`,
+  other: {
+    "msapplication-TileColor": "#ffffff",
+    "msapplication-TileImage": "/favicon/mstile-150x150.png",
+    "msapplication-config": "/favicon/browserconfig.xml",
+  },
+  colorScheme: "light dark",
+  viewport: "width=device-width, initial-scale=1.0",
+  robots: "index, follow",
+  // verification: {
+  //   google: "1234567890",
+  //   yandex: "1234567890",
+  // },
+};
 
 export default async function RootLayout({
   children,
@@ -22,7 +92,6 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <Seo />
       <body className={nunito.className}>
         <Header currentUser={currentUser} />
         {children}
---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Airbnb",
+  url: "https://airbnbclone-vert-eight.vercel.app",
+  ogImage: "https://ui.shadcn.com/og.jpg",
+  description: "Airbnb clone built with Next.js, Tailwind CSS and TypeScript.",
+  links: {
+    twitter: "https://twitter.com/",
+    github: "https://github.com/adham618",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
