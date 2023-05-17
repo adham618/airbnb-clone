f17314685ba6a2df672268168c8fbc4556714fe3
Adham Tarek <adhamtarek291@gmail.com>
2023-05-17 08:41:03 +0300

    fix middleware in prod

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,6 @@
 /* eslint-disable no-console */
 export { default } from "next-auth/middleware";
 
-import type { NextMiddleware, NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-
-export const middleware: NextMiddleware = (req: NextRequest) => {
-  console.log("middleware.ts running");
-  console.log(req.nextUrl.domainLocale);
-  return NextResponse.next();
-};
-
 export const config = {
   // matcher: '/about/:path*',
   matcher: ["/trips", "/reservations", "/properties", "/favorites"],
