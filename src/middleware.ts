c75e1d9191578f5efb31f38dfd0bacc6f70ac7b7
Adham Tarek <adhamtarek291@gmail.com>
2023-05-17 19:37:06 +0300

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,51 @@
-/* eslint-disable no-console */
-export { default } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+  async function middleware(req) {
+    const pathname = req.nextUrl.pathname;
+
+    // Manage route protection
+    const isAuth = await getToken({ req });
+    const isLoginPage = pathname.startsWith("/login");
+
+    const sensitiveRoutes = [
+      "/trips",
+      "/reservations",
+      "/properties",
+      "/favorites",
+    ];
+    const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
+      pathname.startsWith(route)
+    );
+
+    if (isLoginPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+
+      return NextResponse.next();
+    }
+
+    if (!isAuth && isAccessingSensitiveRoute) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+
+    if (pathname === "/") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        return true;
+      },
+    },
+  }
+);
 
 export const config = {
-  // matcher: '/about/:path*',
+  // matchter: ["/", "/login", "/dashboard/:path*"],
   matcher: ["/trips", "/reservations", "/properties", "/favorites"],
 };
