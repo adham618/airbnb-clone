2478ca2b4df1b8b79f41772d3a1bafeb61ba8827
Adham Tarek <adhamtarek291@gmail.com>
2023-05-24 11:22:25 +0300

    fix middleware in prod

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -53,7 +53,7 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 
   debug: process.env.NODE_ENV === "development",
   session: {
